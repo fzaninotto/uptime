1db01f696f9a045ad2e80148dfee6e7510ef92a5
Francois Zaninotto <fzaninotto@gmail.com>
2012-01-04 14:39:51 +0100

    removed example data from source

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ Configure
 
 You must configure the connection settings to the MongoDB database.
 
+You must also add target URIs to monitor. For now, this happens directly in the mongo database:
+
+    > mongo uptime
+    > db.targets.save({url: 'http://www.google.com/index.html', timeout: 1000})
+    > db.targets.save({url: 'http://www.yahoo.com/', timeout: 5000})
+
 Run
 ---
 
--- app.js
+++ app.js
@@ -5,10 +5,6 @@
 var mongoose = require('mongoose'),
     monitor  = require('./lib/monitor');
 
-// models
-var Target = require('./models/target').Target,
-    Ping   = require('./models/ping').Ping;
-
 // configure mongodb
 var mongodbUser = 'root';
 var mongodbPassword = '';
@@ -16,27 +12,6 @@ var mongodbServer = 'localhost';
 var mongodbDatabase = 'uptime';
 mongoose.connect('mongodb://' + mongodbUser + ':' + mongodbPassword + '@' + mongodbServer +'/' + mongodbDatabase);
 
-// clear database
-Target.remove({}, function (err) {
-  if (err) console.dir(err);
-});
-
-// add two targets
-t = new Target();
-t.url = 'http://www.google.com/index.html';
-t.timeout = 200;
-t.save(function (err) {
-  if (err) console.dir(err);
-});
-t = new Target();
-t.url = 'http://www.yahoo.com/';
-t.timeout = 1300;
-t.save(function (err) {
-  if (err) console.dir(err);
-});
-
 // poll targets
 m = monitor.createMonitor(2000);
 m.start();
-
-
