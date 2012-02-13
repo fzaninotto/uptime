19f001f1f020a5a7a4116db22d19ed2ba142b9d9
Francois Zaninotto <fzaninotto@gmail.com>
2012-02-13 22:07:04 +0100

    Update check API for better consistence

--- app/api/routes/check.js
+++ app/api/routes/check.js
@@ -16,10 +16,10 @@ module.exports = function(app) {
     });
   });
   
-  app.get('/check/:name', function(req, res, next) {
-    Check.find({ name: req.params.name }).exclude('qosPerHour').findOne(function(err, check) {
+  app.get('/check/:id', function(req, res, next) {
+    Check.find({ _id: req.params.id }).exclude('qosPerHour').findOne(function(err, check) {
       if (err) return next(err);
-      if (!check) return next(new Error('failed to load check ' + req.params.name));
+      if (!check) return next(new Error('failed to load check ' + req.params.id));
       res.json(check);
     });
   });
