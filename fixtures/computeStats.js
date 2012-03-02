f8fc5d08474891eadc85d367c6aae8bf87d2545b
Francois Zaninotto <fzaninotto@gmail.com>
2012-03-02 18:04:17 +0100

    Add daily and monthly stats to fixtures

--- fixtures/computeStats.js
+++ fixtures/computeStats.js
@@ -6,7 +6,8 @@ var mongoose = require('mongoose'),
 mongoose.connect('mongodb://' + config.user + ':' + config.password + '@' + config.server +'/' + config.database);
 
 // models dependencies
-var Ping   = require('../models/ping');
+var Ping            = require('../models/ping');
+var CheckHourlyStat = require('../models/checkHourlyStat');
 
 var updateLastHourQos = function(callback) {
   console.log('Updating last hour Qos for all checks');
@@ -33,7 +34,43 @@ var updateHourlyQosSinceTheFirstPing = function(callback) {
   });
 }
 
-async.series([updateHourlyQosSinceTheFirstPing, updateLastHourQos], function(err) {
+var updateDailyQosSinceTheFirstPing = function(callback) {
+  Ping.find().asc('date').findOne(function(err, ping) {
+    var date = ping.date.valueOf();
+    var now = Date.now();
+    nbDates = 0;
+    async.whilst(
+      function() { date += 24 * 60 * 60 * 1000; return date < now; },
+      function(cb) {
+        var dateObject = new Date(date);
+        CheckHourlyStat.updateDailyQos(dateObject, cb);
+        nbDates++;
+        console.log('Computing daily stats for ' + dateObject.toUTCString());
+      },
+      callback
+    );
+  });
+}
+
+var updateMonthlyQosSinceTheFirstPing = function(callback) {
+  Ping.find().asc('date').findOne(function(err, ping) {
+    var date = ping.date.valueOf();
+    var now = Date.now();
+    nbDates = 0;
+    async.whilst(
+      function() { date += 28 * 24 * 60 * 60 * 1000; return date < now; },
+      function(cb) {
+        var dateObject = new Date(date);
+        CheckHourlyStat.updateMonthlyQos(dateObject, cb);
+        nbDates++;
+        console.log('Computing monthly stats for ' + dateObject.toUTCString());
+      },
+      callback
+    );
+  });
+}
+
+async.series([updateLastHourQos, updateHourlyQosSinceTheFirstPing, updateDailyQosSinceTheFirstPing, updateMonthlyQosSinceTheFirstPing], function(err) {
   if (err) {
     console.dir(err);
   } else {
