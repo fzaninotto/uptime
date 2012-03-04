bdee0a8f97c5c44028e4e4b53b196c808c0875d4
Francois Zaninotto <fzaninotto@gmail.com>
2012-03-04 15:47:14 +0100

    Add persistence for the check up / check down events

---
+++ models/checkEvent.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+var CheckEvent = new Schema({
+    timestamp   : { type: Date, default: Date.now }
+  , check       : Schema.ObjectId
+  , isGoDown    : Boolean // true if the event is for a check going DOWN, false if it is for a check going back UP
+  // for error events, more details need to be persisted
+  , downtime    : Number
+});
+CheckEvent.index({ check: 1, timestamp: -1 });
+
+module.exports = mongoose.model('CheckEvent', CheckEvent);
