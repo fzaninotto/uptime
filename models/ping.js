a4953a7df2fe54c5b0f63f50da2eba1c3809ab35
Francois Zaninotto <fzaninotto@gmail.com>
2012-05-16 14:43:12 +0200

    Fix autocleanup

--- models/ping.js
+++ models/ping.js
@@ -119,7 +119,7 @@ Ping.statics.updateLast24HoursQos = function(callback) {
 
 Ping.statics.cleanup = function(maxAge, callback) {
   oldestDateToKeep = new Date(Date.now() - (maxAge ||  3 * 31 * 24 * 60 * 60 * 1000));
-  this.find({ timestamp: { $lt: oldestDateToKeep } }).remove(callback);
+  this.find({ timestamp: { $lt: new Date(oldestDateToKeep) } }).remove(callback);
 }
 
 module.exports = mongoose.model('Ping', Ping);
