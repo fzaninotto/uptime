1dd963dcbd1cdd6b9642477573041a1927c12e09
obfuscurity <jason@dixongroup.net>
2012-10-16 22:44:03 -0400

    more user-friendly message, combine with check info

--- plugins/webhook/index.js
+++ plugins/webhook/index.js
@@ -17,21 +17,40 @@ exports.init = function(enableNewEvents, enableNewPings) {
   if (enableNewPings)  registerNewPingsWebhook();
 };
 
-var httpRequest = function(data) {
+var constructMessage = function(check, result, cb) {
+  var message = {
+    name: check.name,
+    check_id: result.check,
+    event_id: result._id,
+    duration: result.time,
+    downtime: result.downtime,
+    message: result.message,
+    details: result.details,
+    error: result.error,
+    url: check.url,
+    tags: result.tags,
+    timestamp: result.timestamp,
+    isUp: result.isUp,
+    isResponsive: result.isResponsive
+  };
+  cb(message);
+};
+
+var httpRequest = function(msg) {
   var req = http.request(http_options, function(res) {
     res.setEncoding('utf8');
   });
   req.on('error', function(e) {
     console.log('problem with request: ' + e.message);
   });
-  req.write(JSON.stringify(data));
+  req.write(JSON.stringify(msg));
   req.end();
 };
 
 var registerNewPingsWebhook = function() {
   Ping.on('afterInsert', function(ping) {
     ping.findCheck(function(err, check) {
-      httpRequest(ping);
+      constructMessage(check, ping, httpRequest);
     });
   });
 };
@@ -39,7 +58,7 @@ var registerNewPingsWebhook = function() {
 var registerNewEventsWebhook = function() {
   CheckEvent.on('afterInsert', function(checkEvent) {
     checkEvent.findCheck(function(err, check) {
-      httpRequest(checkEvent);
+      constructMessage(check, checkEvent, httpRequest);
     });
   });
 };
