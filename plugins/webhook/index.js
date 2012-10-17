21e514a251471857c65dc63b496546646bd3c2a8
obfuscurity <jason@dixongroup.net>
2012-10-16 20:58:05 -0400

    refactor webhook plugin; default to reporting check events only

--- plugins/webhook/index.js
+++ plugins/webhook/index.js
@@ -1,6 +1,6 @@
-
-var http = require('http');
 var Ping = require('../../models/ping');
+var CheckEvent = require('../../models/checkEvent');
+var http = require('http');
 
 var config = require('config').webhook;
 var http_options = {
@@ -10,23 +10,36 @@ var http_options = {
   method: 'POST'
 };
 
-var req = http.request(http_options, function(res) {
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk) if (config.verbose);
+exports.init = function(enableNewEvents, enableNewPings) {
+  if (typeof enableNewEvents == 'undefined') enableNewEvents = true;
+  if (typeof enableNewPings == 'undefined') enableNewPings = false;
+  if (enableNewEvents) registerNewEventsWebhook();
+  if (enableNewPings)  registerNewPingsWebhook();
+};
+
+var httpRequest = function(data) {
+  var req = http.request(http_options, function(res) {
+    res.setEncoding('utf8');
   });
-});
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+  req.write(JSON.stringify(data));
+  req.end();
+};
 
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
-
-req.end();
-
-exports.init = function() {
+var registerNewPingsWebhook = function() {
   Ping.on('afterInsert', function(ping) {
     ping.findCheck(function(err, check) {
-      req.write(JSON.stringify(ping));
+      httpRequest(ping);
+    });
+  });
+};
+
+var registerNewEventsWebhook = function() {
+  CheckEvent.on('afterInsert', function(checkEvent) {
+    checkEvent.findCheck(function(err, check) {
+      httpRequest(checkEvent);
     });
   });
 };
