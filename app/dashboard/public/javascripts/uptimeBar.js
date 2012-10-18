1a036f3ff4397a2173e6a8e4bd620c6ebb209549
Francois Zaninotto <fzaninotto@gmail.com>
2012-10-18 10:11:42 +0200

    Switch uptime bar drawing to absolute div positionning
    Avoids rounding issues that would remove smaller intervals and reduce
    the overall size of the uptime bar when many small outages appear.

--- app/dashboard/public/javascripts/uptimeBar.js
+++ app/dashboard/public/javascripts/uptimeBar.js
@@ -1,29 +1,16 @@
 var uptimeBar = (function(){
-  var barPart = function(type, begin, end, duration) {
-    return '<div class="' + type + '" '
-              + 'style="width:' + (end - begin) / duration * 100 +'%" ' 
-              + 'title="' + type + ' from '+ moment(begin).format('LLL') + ' to ' + moment(end).format('LLL') + '">'
+  var outageBar = function(begin, end, origin, duration) {
+    return '<div class="down" '
+              + 'style="left:' + (begin - origin) / duration * 100 + '%;width:' + (end - begin) / duration * 100 + '%" ' 
+              + 'title="' + moment.duration(end-begin).humanize() + ' outage from '+ moment(begin).format('LLL') + ' to ' + moment(end).format('LLL') + '">'
          + '</div>';
   }
-  var uptimeBar = function(begin, end, periods) {
+  var uptimeBar = function(from, to, periods) {
     var ret = '<div class="uptimeBar">';
+    var duration = to - from;
     var nbPeriods = periods.length;
-    if (nbPeriods == 0) {
-      ret += '<div class="up" style="width:100%"></div></div>';
-      return ret;
-    }
-    var duration = (end - begin) * 1.02; // avoid rounding issues;
-    if (periods[0][0] != begin) {
-      ret += barPart('up', begin, periods[0][0], duration);
-    }
     for (var i = 0; i < nbPeriods; i++) {
-      ret += barPart('down', periods[i][0], periods[i][1], duration);
-      if (i < nbPeriods - 1 ) {
-        ret += barPart('up', periods[i][1], periods[i+1][0], duration);
-      }
-    }
-    if (periods[nbPeriods - 1][1] != end) {
-      ret += barPart('up', periods[nbPeriods-1][1], end, duration);
+      ret += outageBar(periods[i][0], periods[i][1], from, duration);
     }
     ret += '</div>';
     return ret;
