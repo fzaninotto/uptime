aef36aa51de3dc2156bb65a7d831ef4bc8e5da24
Francois Zaninotto <fzaninotto@gmail.com>
2012-11-19 08:48:12 +0100

    Control MongoDB server version to avoid strange errors
    Refs #48

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,6 @@
 var mongoose   = require('mongoose');
 var config     = require('config');
+var semver     = require('semver')
 
 // configure mongodb
 mongoose.connect(config.mongodb.connectionString || 'mongodb://' + config.mongodb.user + ':' + config.mongodb.password + '@' + config.mongodb.server +'/' + config.mongodb.database);
@@ -7,5 +8,14 @@ mongoose.connection.on('error', function (err) {
   console.error('MongoDB error: ' + err.message);
   console.error('Make sure a mongoDB server is running and accessible by this application')
 });
+mongoose.connection.on('open', function (err) {
+  mongoose.connection.db.admin().serverStatus(function(err, data) { 
+    if (!semver.satisfies(data.version, '>=2.1.0')) {
+      console.error('Error: Uptime requires MongoDB v2.1 minimum. The current MongoDB server uses only '+ data.version);
+      process.exit(1);
+    };
+  });
+});
+
 
 module.exports = mongoose;
