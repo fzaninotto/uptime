a92476883de236fd750533e9d20bbac500b65c9b
vagrant <vagrant@precise64.(none)>
2012-12-21 16:41:10 +0000

    small changes

--- plugins/boxcar/index.js
+++ plugins/boxcar/index.js
@@ -1,22 +1,23 @@
 /**
- * Prowl plugin
+ * Boxcar plugin
  *
  * Notifies all events (up, down, paused, restarted) by email
  *
  * To enable the plugin, call init() from plugins/index.js
  *   exports.init = function() {
- *     require('./prowl').init();
+ *     require('./boxcar').init();
  *   }
  *
  * Example configuration
- *   prowl:
- *     key:         36e605fd9717f5b28967ca546593bbe84abb63b1   
- *     event:
- *       up:        true
- *       down:      true
- *       paused:    false
- *       restarted: false
- *     dashboardUrl: 'http://localhost:8082'
+ * boxcar:
+ *   email:                   'your_registered_boxcar_email@mail.com'
+ *   key: 'AagXreNW1r8Ib9HQHMnO'
+ *   secret: 'qSCpuH1ep6wLhU0MLPxbFZWqe0io7ighMKLXTTOO'
+ *   event:
+ *     up:        true
+ *     down:      true
+ *     paused:    true
+ *     restarted: true
  */
  
 
@@ -36,10 +37,10 @@ exports.init = function() {
       		if (err) return console.error(err);
 
       		var text = check.name + ' has been ' + checkEvent.message;
-      		//+ 'failed with this error : '+checkEvent.details
       		
-      		console.log("boxcar : " + text);
-      		console.log(config.email + ',' + config.password);
+      		//+ 'failed with this error : '+checkEvent.details
+      		//console.log("boxcar : " + text);
+      		//console.log(config.email + ',' + config.password);
       		
       		var boxcar = require('boxcar');
 			var provider = new boxcar.Provider(config.key, config.secret);
