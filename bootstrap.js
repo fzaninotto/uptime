e61f0aebde0bd5420fc44eee71973a6a69bdfe14
Francois Zaninotto <fzaninotto@gmail.com>
2013-01-13 15:52:58 +0100

    Stop the server if MongoDB is not started

--- bootstrap.js
+++ bootstrap.js
@@ -6,7 +6,8 @@ var semver     = require('semver')
 mongoose.connect(config.mongodb.connectionString || 'mongodb://' + config.mongodb.user + ':' + config.mongodb.password + '@' + config.mongodb.server +'/' + config.mongodb.database);
 mongoose.connection.on('error', function (err) {
   console.error('MongoDB error: ' + err.message);
-  console.error('Make sure a mongoDB server is running and accessible by this application')
+  console.error('Make sure a mongoDB server is running and accessible by this application');
+  process.exit(1);
 });
 mongoose.connection.on('open', function (err) {
   mongoose.connection.db.admin().serverStatus(function(err, data) { 
